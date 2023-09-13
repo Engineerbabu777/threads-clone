78d9fa1ad8098da5fd6ccbf049144f6379b0529c
Engineerbabu777 <awaismumtaz0099@gmail.com>
2023-09-13 19:13:55 +0500

    thread-v0.1.1

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
       serverActions: true,
       serverComponentsExternalPackages: ["mongoose"],
     },
+    typescript:{
+         ignoreBuildErrors:true,
+    },
     eslint: {
       // Warning: This allows production builds to successfully complete even if
       // your project has ESLint errors.
